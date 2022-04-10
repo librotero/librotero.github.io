894d8f0115f25b0bcc02c0c7ad15937949b2a7be
Eduardo <70926975+librotero@users.noreply.github.com>
2022-04-10 14:27:11 -0300

    Update Header.jsx

--- portfoliojesus/src/Componenetes/Header.jsx
+++ portfoliojesus/src/Componenetes/Header.jsx
@@ -4,6 +4,7 @@ export default function Header(props){
     return(
         <div>
             <div className={s.parrallax}>
+                
                 <img src={"https://as2.ftcdn.net/v2/jpg/02/78/37/47/1000_F_278374738_ypRn0utOVnebuhmpSrDiwkzFsdqEm0aa.jpg"}/>
             </div>
             <div>
